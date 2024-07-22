fc7bc2002245f01f394c64f930db4e2242aa698e
Edward <edward.bock@palasthotel.de>
2024-07-22 20:17:18 +0200

    author image fix

--- nextjs/src/app/authors/page.tsx
+++ nextjs/src/app/authors/page.tsx
@@ -5,7 +5,6 @@ import Destinations from "@/lib/destinations";
 
 export default async function Authors_Page(){
 
-
     const users = await wpFetchUsers();
 
     return (
@@ -13,16 +12,22 @@ export default async function Authors_Page(){
             <h1>Authors</h1>
             <ul>
                 {users?.data?.map(user => {
+                    const avatarUrl = user.avatar_urls["96"]
                     return (
                         <li key={user.id}>
                             <Link href={Destinations.author({slug: user.slug})}>
-                                <Image
-                                    src={user.avatar_urls["96"]}
-                                    alt={""}
-                                    width={96}
-                                    height={96}
-                                />
-                                <br/>
+                                {avatarUrl ?
+                                    <>
+                                        <Image
+                                            src={avatarUrl}
+                                            alt={""}
+                                            width={96}
+                                            height={96}
+                                        />
+                                        <br/>
+                                    </>
+                                    : null
+                                }
                                 {user.name}
                             </Link>
                         </li>
